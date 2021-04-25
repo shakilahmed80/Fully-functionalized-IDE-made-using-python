0e4a817cafde9ad44b04bd5e170bc0732f9cadbd
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2021-04-26 00:43:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+## Setting the div
 from tkinter import *
 compiler = Tk()
 compiler.title('Shakils IDE')
 
+def run():
+    print('Your Code Will Be Run')
+
 ## We need to runbar (5line code)
-
-
 
 menu_bar = Menu(compiler)
 run_bar =Menu(menu_bar,tearoff=0)
-run_bar.add_command(label='Run')
+run_bar.add_command(label='Run',command = run)
 menu_bar.add_cascade(label = 'Run',menu = run_bar)
 compiler.config(menu=menu_bar)
 
@@ -16,5 +18,5 @@ editor = Text()
 editor.pack()
 compiler.mainloop()
 
-## Setting the div
+
 
