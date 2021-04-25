8018120d4c58cf7d618955cd9279cc79749bda6d
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2021-04-26 00:37:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from tkinter import *
 compiler = Tk()
 compiler.title('Shakils IDE')
 
-## We need to runbar (4line code)
+## We need to runbar (5line code)
+
+
 
 menu_bar = Menu(compiler)
+run_bar =Menu(menu_bar,tearoff=0)
 run_bar.add_command(label='Run')
-menu_bar.add_casecade(label = 'Run',menu = run_bar)
+menu_bar.add_cascade(label = 'Run',menu = run_bar)
 compiler.config(menu=menu_bar)
 
 editor = Text()
 editor.pack()
 compiler.mainloop()
 
-## Setting the div 
+## Setting the div
 
