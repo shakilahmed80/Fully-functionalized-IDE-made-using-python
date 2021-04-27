9b69c8b09cb2079a4f0a364ded5a0015579fd084
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2021-04-27 20:10:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,82 @@
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename, askopenfilenames, askopenfilename
 
+import subprocess 
+
 compiler = Tk()
 compiler.title('Shakils IDE')
-file_path = ''
+file_path = ''   
+
+
+
 
 def set_file_path(path):
-    
-    global file_path 
+    global file_path
     file_path = path
 
 
 def open_file():
-    path = askopenfilename(filetypes = [('Python Files ', '*.py')])
-    with open (path,'r') as file :
-         code = file.read()
-         editor.delete('1.0',END)
-         editor.insert('1.0',code)
-         set_file_path(path)
+    path = askopenfilename(filetypes=[('Python Files ', '*.py')])
+    with open(path, 'r') as file:
+        
+        code = file.read()
+        editor.delete('1.0', END)
+
+     
+
+        editor.insert('1.0', code)
+        set_file_path(path)
+
+
 
 def save_as():
-    
-    if file_path == '' :
 
-        path = asksaveasfilename(filetypes = [('Python Files ', '*.py')])
-    
-    else :
-        path = file_path
-    
-    with open (path,'w') as file :
-         code = editor.get('1.0',END)
-         file.write(code)
+    if file_path == '' :
+       path = asksaveasfilename(filetypes=[('Python Files ', '*.py')])
+
+    else:  
+        file = file_path
+    with open(path,'w') as file:
+        code = editor.get('1.0', END)
+        file.write(code)
 
 
 def run():
-    code = editor.get('1.0',END)
-    exec (code)
-    print('Your Code Will Be Run')
+   
+
+    
+      command = f'python {file_path}'
+
+     
+      process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+
+
+
+
 
 
 menu_bar = Menu(compiler)
 
+file_bar = Menu(menu_bar, tearoff=0)
+file_bar.add_command(label='Open File', command=open_file)
+file_bar.add_command(label='Open Folder', command=run)
+file_bar.add_command(label='Save', command=save_as)  
+file_bar.add_command(label='Save As', command=save_as)
+file_bar.add_command(label='Exit', command=exit)
+menu_bar.add_cascade(label='File', menu=file_bar)
 
-file_bar =Menu(menu_bar,tearoff=0)
-file_bar.add_command(label='Open File',command = open_file)
-file_bar.add_command(label='Open Folder',command = run)
-file_bar.add_command(label='Save',command = save_as)
-file_bar.add_command(label='Save As',command = save_as)
-file_bar.add_command(label='Exit',command = exit)
-menu_bar.add_cascade(label = 'File',menu = file_bar)
-
-
-
-run_bar =Menu(menu_bar,tearoff=0)
-run_bar.add_command(label='Run',command = run)
-menu_bar.add_cascade(label = 'Run',menu = run_bar)
-
+run_bar = Menu(menu_bar, tearoff=0)
+run_bar.add_command(label='Run', command=run)
+menu_bar.add_cascade(label='Run', menu=run_bar)
 
 compiler.config(menu=menu_bar)
 
-editor = Text()
+editor = Text() 
 editor.pack()
+
+code_output = Text(height = 10) 
+code_output.pack()
 compiler.mainloop()
 
 
