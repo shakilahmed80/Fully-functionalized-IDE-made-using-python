8a26061e745ce4cf64ec2d30617d24251686b423
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2021-04-27 17:32:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-## Setting the div
 from tkinter import *
-
-##this is for pop up save file window and open file name
 from tkinter.filedialog import asksaveasfilename, askopenfilenames, askopenfilename
 
 compiler = Tk()
 compiler.title('Shakils IDE')
-
-file_path = ' '
 
 def set_file_path(path):
     
@@ -18,35 +13,24 @@ def set_file_path(path):
 def open_file():
     path = askopenfilename(filetypes = [('Python Files ', '*.py')])
     with open (path,'r') as file :
-## here w means that write
          code = file.read()
          editor.delete('1.0',END)
 
-## 1.0 MEANS INDEX 1.0 AND END MEANS TILL THE END OF THE DOCUMENT
-
          editor.insert('1.0',code)
          set_file_path(path)
-## code means read form that file
+
 def save_as():
-     
-    if file_path == '':
     path = asksaveasfilename(filetypes = [('Python Files ', '*.py')])
-   
-   else:
-       path = file_path
-   
     with open (path,'w') as file :
          code = editor.get('1.0',END)
          file.write(code)
 
 
 def run():
-    ## here i am capturing all thing like forloop,whileloop all staff
     code = editor.get('1.0',END)
     exec (code)
     print('Your Code Will Be Run')
 
-## We need to runbar (5line code)
 
 menu_bar = Menu(compiler)
 
@@ -54,7 +38,7 @@ menu_bar = Menu(compiler)
 file_bar =Menu(menu_bar,tearoff=0)
 file_bar.add_command(label='Open File',command = open_file)
 file_bar.add_command(label='Open Folder',command = run)
-file_bar.add_command(label='Save',command = save_as)
+file_bar.add_command(label='Save',command = run)
 file_bar.add_command(label='Save As',command = save_as)
 file_bar.add_command(label='Exit',command = exit)
 menu_bar.add_cascade(label = 'File',menu = file_bar)
